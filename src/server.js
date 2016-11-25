22f72818cf4bbb5e7e8caed145f523ad7c38ba86
abdenasser <nasser.elidrissi065@gmail.com>
2016-11-25 18:10:52 +0000

    some error handling

--- src/server.js
+++ src/server.js
@@ -13,5 +13,19 @@ server.route( {
   handler: (request, reply) => {
     reply('Hello world!');
   }
-  
+
+});
+
+server.start(err => {
+
+    if (err) {
+
+        // Fancy error handling here
+        console.error( 'Error was handled!' );
+        console.error( err );
+
+    }
+
+    console.log( `Server started at ${ server.info.uri }` );
+
 });
