dff11186ef74192fb18645dcf48592992fdb7fbd
abdenasser <nasser.elidrissi065@gmail.com>
2016-11-25 18:05:00 +0000

    adding a simple route

--- src/server.js
+++ src/server.js
@@ -5,3 +5,13 @@ const server = new Hapi.Server();
 server.connection( {
   port: 8080
 });
+
+server.route( {
+
+  method: 'GET',
+  path: '/hello',
+  handler: (request, reply) => {
+    reply('Hello world!');
+  }
+  
+});
