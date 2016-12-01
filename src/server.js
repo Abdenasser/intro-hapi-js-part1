62cf56673029681ce6a796c00416ebc554707200
abdenasser <nasser.elidrissi065@gmail.com>
2016-12-01 21:26:24 +0000

    Authentication with hapi-auth-basic

--- src/server.js
+++ src/server.js
@@ -1,53 +1,47 @@
-import Hapi from 'hapi';
+'use strict';
+
+const Bcrypt = require('bcrypt');
+const Hapi = require('hapi');
+const Basic = require('hapi-auth-basic');
 
 const server = new Hapi.Server();
+server.connection({ port: 3000 });
 
-server.connection( {
-  host: 'localhost',
-  port: 8080
-});
+const users = {
+    john: {
+        username: 'john',
+        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+        name: 'John Doe',
+        id: '2133d32a'
+    }
+};
 
-server.register(require('inert'), (err) => {
+const validate = function (request, username, password, callback) {
+    const user = users[username];
+    if (!user) {
+        return callback(null, false);
+    }
+
+    Bcrypt.compare(password, user.password, (err, isValid) => {
+        callback(err, isValid, { id: user.id, name: user.name });
+    });
+};
+
+server.register(Basic, (err) => {
 
     if (err) {
         throw err;
     }
 
-    server.route( {
-
-      method: 'GET',
-      path: '/',
-      handler: (request, reply) => {
-        reply('Hello world!');
-      }
-
-    });
-
-//    server.route( {
-//
-//      method: 'GET',
-//      path: '/{name}',
-//      handler: (request, reply) => {
-//        reply(`Hello ${encodeURIComponent(request.params.name)} !`);
-//      }
-//
-//    });
-
-    server.route( {
-
-      method: ['POST', 'PUT'],
-      path: '/gotcha',
-      handler: (request, reply) => {
-        reply(`You sent me a ${encodeURIComponent(request.method)} method!`)
-      }
-
-    });
-
+    server.auth.strategy('simple', 'basic', { validateFunc: validate });
     server.route({
         method: 'GET',
-        path: '/screenshot',
-        handler: function (request, reply) {
-            reply.file('public/screenshot.png');
+        path: '/',
+        config: {
+            auth: 'simple',
+            handler: function (request, reply) {
+                reply('hello, ' + request.auth.credentials.name);
+            }
         }
     });
 
@@ -57,6 +51,6 @@ server.register(require('inert'), (err) => {
             throw err;
         }
 
-        console.log('Server running at:', server.info.uri);
+        console.log('server running at: ' + server.info.uri);
     });
 });
